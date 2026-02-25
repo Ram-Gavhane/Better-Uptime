20dca06b85628d8995d08c7632c052c6c08548e7
Ram Gavhane <ramgavhane28@gmail.com>
2026-02-25 16:08:50 +0530

    completed CRUD functionality

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -2,6 +2,7 @@ import prismaClient from "@repo/db";
 import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "./config";
+import middleware from "./middleware";
 
 const app = express();
 
@@ -47,6 +48,70 @@ app.post("/signin", async (req, res) => {
     res.json({
         message: "User signed in successfully",
         token
+    });
+});
+
+app.post("/add-website", middleware, async (req, res) => {
+    const { url, userId } = req.body;
+    const user = await prismaClient.user.findUnique({
+        where: {
+            id: userId,
+        },
+    });
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    try {
+        const response = await prismaClient.website.create({
+            data: {
+                url,
+                userId: userId,
+            },
+        });
+        res.json({
+            message: "Website added successfully",
+            response
+        });
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+});
+
+app.get("/get-websites", middleware, async (req, res) => {
+    const { userId } = req.body;
+    const user = await prismaClient.user.findUnique({
+        where: {
+            id: userId,
+        },
+    });
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    const websites = await prismaClient.website.findMany({
+        where: {
+            userId: userId,
+        },
+    });
+    res.json({
+        message: "Websites fetched successfully",
+        websites
+    });
+});
+
+app.get("/website/:id", middleware, async (req, res) => {
+    const id = req.params.id as string;
+    const website = await prismaClient.website.findUnique({
+        where: {
+            id
+        },
+    });
+    if (!website) {
+        return res.status(404).json({ message: "Website not found" });
+    }
+    res.json({
+        message: "Website fetched successfully",
+        website
     });
 });
 
