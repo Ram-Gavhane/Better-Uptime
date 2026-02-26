0f088a16dcd377862ed88dfad697299f2ac4ca07
Ram Gavhane <ramgavhane28@gmail.com>
2026-02-26 19:08:17 +0530

    producer added

---
+++ apps/producer/index.ts
@@ -0,0 +1,23 @@
+import prismaClient from "@repo/db";
+import { xAddBulk } from "@repo/redisstreams";
+
+async function main() {
+    const websites = await prismaClient.website.findMany({
+        select: {
+            url: true,
+            id: true,
+        }
+    });
+    console.log(websites);
+
+    await xAddBulk(websites.map(website => ({
+        url: website.url,
+        id: website.id,
+    })));
+}
+
+setInterval(() => {
+    main();
+}, 3 * 60 * 1000);
+
+main();
--- packages/redisstreams/index.ts
+++ packages/redisstreams/index.ts
@@ -1,6 +1,22 @@
 import { createClient } from 'redis';
 
-const client = createClient()
+const client = await createClient()
     .on('error', err => console.log('Redis Client Error', err))
     .connect();
 
+type websiteData = {
+    url: string;
+    id: string;
+}
+
+
+async function xAddBulk(websites: websiteData[]) {
+    for (let i = 0; i < websites.length; i++) {
+        await client.xAdd('website:stream', '*', {
+            url: websites[i]!.url,
+            id: websites[i]!.id
+        })
+    }
+}
+
+export { xAddBulk };
